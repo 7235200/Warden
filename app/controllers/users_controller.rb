4956872b95c90022e867e559ebb5093443f2b2b6
dimadima <7235200@gmail.com>
2014-12-08 13:46:24 +0000

    s<---

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       # redirect_to @user
-      redirect_to transaction_path
+      redirect_to transactions_path
     else
       render 'new'
     end
